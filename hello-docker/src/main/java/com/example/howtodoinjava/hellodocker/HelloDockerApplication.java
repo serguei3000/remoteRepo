4ef31cca33f620c5e9f088699496b7e076e0599e
serguei3000 <serguei3000@yandex.ru>
2019-11-20 12:19:44 +0300

    Update HelloDockerApplication.java
    third commit

--- hello-docker/src/main/java/com/example/howtodoinjava/hellodocker/HelloDockerApplication.java
+++ hello-docker/src/main/java/com/example/howtodoinjava/hellodocker/HelloDockerApplication.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 //second commit
 
+//third commit (on gitbub)
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
